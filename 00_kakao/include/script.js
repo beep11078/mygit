e6f09e1b91acf5cdd414a2ba62cdfa3c87a3fcff
beep11078 <beep11078@gmail.com>
2024-10-23 23:09:11 +0900

    1023 kakao JS

--- 00_kakao/include/script.js
+++ 00_kakao/include/script.js
@@ -1,16 +1,34 @@
+// 스크롤시 메뉴 하단 경계선
+const header = document.querySelector('header');
 
-let toggle = document.querySelector('.btn_dark')
-
-toggle.addEventListener('click', (e) => {
-    // e: 이벤트가 발생했을 때 그 처리를 담당하는 콜백 함수-의 객체 (이벤트 객체)
-    // toggle키에 클릭 이벤트가 발생했을 때, 
-    const html = document.querySelector('html')
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'dark_mode'
-        // 이벤트의 타겟(사용자가 클릭한 실제 요소)인 toggle키의 HTML 태그의 안에 'D'를 넣는다.
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
     } else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'light_mode'
+        header.classList.remove('scrolled');
     }
+});
+
+const menuTitle = document.querySelector('.menuTitle');
+const subMenu = document.querySelector('.subMenu');
+
+menuTitle.addEventListener('click', () => {
+    subMenu.classList.toggle('subActive');
 })
+
+
+// // 다크모드
+// let toggle = document.querySelector('.btn_dark')
+// let bubble = document.querySelectorAll('button')
+
+// toggle.addEventListener('click', (e) => {
+//     const html = document.querySelector('html')
+//     if (html.classList.contains('dark')) {
+//         html.classList.remove('dark')
+//         e.target.innerHTML = 'dark_mode'
+//     } else {
+//         html.classList.add('dark')
+//         e.target.innerHTML = 'light_mode'
+//         bubble.forEach(elem => { e.style.filter = 'invert(100%)'});
+//     }
+// })
