7c96f1620f83e391dc03e605884f0e83c5e46b38
beep11078 <beep11078@gmail.com>
2024-11-25 15:59:38 +0900

    1125

--- wooribank/include/common.js
+++ wooribank/include/common.js
@@ -5,11 +5,30 @@ let mySlide = new Swiper('.swiper', {
     autoplay : {
         delay : 1000,
     },
+    loop : true,
     navigation : {
-        prevEl : '.slider .prev',
-        nextEl : '.slider .next'
+        prevEl : '.sliderCtr .prev',
+        nextEl : '.sliderCtr .next'
     },
     pagination : {
-        el : '.slider .mypager',
+        el : '.sliderCtr .mypager',
     },
+});
+
+
+const btn2 = document.querySelectorAll('.control li');
+btn2.forEach(function(el, i){
+    el.addEventListener('click', function(e){
+        if(e.target.classList.contains('play')) {
+            btn2[0].classList.add('hide');
+            btn2[1].classList.remove('hide');
+            // 스와이퍼 오토플레이 실행
+            mySlide.autoplay.start();
+        } else if (e.target.classList.contains('pause')) {
+            btn2[1].classList.add('hide');
+            btn2[0].classList.remove('hide');
+            // 스와이퍼 오토플레이 중지
+            mySlide.autoplay.stop();
+        }
+    });
 });
