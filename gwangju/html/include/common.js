531c82b485aebea04cff3c335be4ac95f820e121
beep11078 <beep11078@gmail.com>
2024-12-20 12:40:18 +0900

    1220

---
+++ gwangju/html/include/common.js
@@ -0,0 +1,13 @@
+let mySlide = new Swiper(".main-banner__swiper", {
+    autoplay: {
+        delay: 1000,
+    },
+    loop: true,
+    // navigation: {
+    //     prevEl: ".sliderCtr .prev",
+    //     nextEl: ".sliderCtr .next",
+    // },
+    // pagination: {
+    //     el: ".sliderCtr .mypager",
+    // },
+});
