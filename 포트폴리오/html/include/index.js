a891fbc568641c6c3050118697d460c6fcdb02f3
beep11078 <beep11078@gmail.com>
2024-12-25 13:31:54 +0900

    1224

--- 포트폴리오/html/include/index.js
+++ 포트폴리오/html/include/index.js
@@ -1,16 +1,27 @@
+const memoji = document.querySelector('.profile__memoji');
+
+const closeBtn = document.querySelector(".profile__modalbox-close");
+const modal = document.querySelector(".profile__modal");
+
+
+memoji.addEventListener("click", (e) => {
+    modal.style.display = 'block';
+})
+
 function copy(elementId) {
-    const text = document.getElementById(elementId).innerText;
-    navigator.clipboard.writeText(text).then(() => {
-        alert('복사되었습니다 : ' + text);
-    }).catch(err => {
-        console.error('복사 실패', err);
-    });
+    const copyText = document.getElementById(elementId).innerText;
+    navigator.clipboard
+        .writeText(text)
+        .then(() => {
+            alert("복사되었습니다 : " + copyText);
+        })
+        .catch((err) => {
+            console.error("복사 실패", err);
+        });
 }
 
-const closeBtn = document.querySelector('.profile__modalbox-close');
-const modal = document.querySelector('.profile__modal');
-
-closeBtn.addEventListener('click', (e) => {
+closeBtn.addEventListener("click", (e) => {
     // searchBtn이 클릭된 경우
-    modal.style.display = 'none';
+    modal.style.display = "none";
 });
+
