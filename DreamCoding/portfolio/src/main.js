9b954826ba78a061f0292188cb828fed90566ee3
beep11078 <beep11078@gmail.com>
2025-01-05 19:27:40 +0900

    JS-Home

--- DreamCoding/portfolio/src/main.js
+++ DreamCoding/portfolio/src/main.js
@@ -1,5 +1,4 @@
 // Header 페이지 아래로 스크롤시 다크 스타일링 적용
-
 const header = window.document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
@@ -9,4 +8,12 @@ document.addEventListener('scroll', function(){
     } else {
         header.classList.remove('header--dark');
     }
+})
+
+
+// Home 섹션 스크롤시 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 })
