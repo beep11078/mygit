e111170faa706aa575d9721cb50603c1ddeb201c
beep11078 <beep11078@gmail.com>
2025-01-08 16:47:28 +0900

    HTML-modal

--- Portfolio/html/include/index.js
+++ Portfolio/html/include/index.js
@@ -1,17 +1,17 @@
 let mySwiper1 = new Swiper("#portfolio1 .swiper-container", {
-    autoplay: {
-        delay: 2000,
-    },
-    slidesPerView: 1,
-    spaceBetween: 30,
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
 });
 
 let mySwiper2 = new Swiper("#portfolio2 .swiper-container", {
-    autoplay: {
-        delay: 2000,
-    },
-    slidesPerView: 3,
-    spaceBetween: 30,
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
 });
 
 const memoji = document.querySelector(".profile__memoji");
@@ -20,24 +20,30 @@ const closeBtn = document.querySelector(".profile__modalbox-close");
 const modal = document.querySelector(".profile__modal");
 
 memoji.addEventListener("click", (e) => {
-    modal.style.display = "block";
-    document.body.style.overflow = "hidden";
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
 });
 
-function copy(elementId) {
-    const copyText = document.getElementById(elementId).innerText;
+// 복사 함수
+function copy(targetId) {
+  const targetElement = document.getElementById(targetId); // ID로 요소 선택
+  if (targetElement) {
+    const textToCopy = targetElement.textContent || targetElement.innerText; // 텍스트 가져오기
     navigator.clipboard
-        .writeText(text)
-        .then(() => {
-            alert("복사되었습니다 : " + copyText);
-        })
-        .catch((err) => {
-            console.error("복사 실패", err);
-        });
+      .writeText(textToCopy)
+      .then(() => {
+        alert("복사되었습니다: " + textToCopy);
+      })
+      .catch((err) => {
+        console.error("복사 실패:", err);
+      });
+  } else {
+    console.error("복사할 요소를 찾을 수 없습니다: " + targetId);
+  }
 }
 
-closeBtn.addEventListener("click", (e) => {
-    // searchBtn이 클릭된 경우
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
-});
+// closeBtn.addEventListener("click", (e) => {
+//   // searchBtn이 클릭된 경우
+//   modal.style.display = "none";
+//   document.body.style.overflow = "auto";
+// });
