16d02362592785393545f2cd9fb06edf1aa00eca
beep11078 <beep11078@gmail.com>
2025-01-14 13:54:30 +0900

    JS-class, getter

---
+++ Javascript/2_class_and_oop/1_class_keywords.js
@@ -0,0 +1,33 @@
+/**
+ * class : 클래스는 객체지향 프로그래밍에서 특정 객체(인스턴스)를 생성하기 위한 변수와 메소드(객체 안의 함수)를 정의하는 일종의 틀이다. => 정보를 일반화해서 정리하는 틀
+*/
+
+class MyBias {
+  // name = '정대만';
+  // birth = 522;
+  // 프로퍼티 정의 생략 가능
+
+  constructor(name, birth){
+    this.name = name;
+    this.birth = birth;
+    // this : 현재 인스턴스 
+  }
+
+  sayName(){
+    return `그래, 내 이름은 ${this.name}...`;
+  }
+}
+
+const daeMan = new MyBias('정대만', 522);
+console.log(daeMan);
+const haJin = new MyBias('서하진', 612);
+console.log(haJin);
+
+console.log(daeMan.name);
+console.log(daeMan.birth);
+
+console.log(daeMan.sayName());
+console.log(haJin.sayName());
+
+console.log(typeof MyBias); // class는 함수의 일종
+console.log(typeof daeMan); // class로 생성한 instance는 객체object이다.
---
+++ Javascript/2_class_and_oop/2_getter_n_setter.js
@@ -0,0 +1,60 @@
+/**
+ * Getter and Setter
+ */
+
+class MyBias {
+  name;
+  birth;
+
+  constructor(name, birth) {
+    this.name = name;
+    this.birth = birth;
+  }
+
+  /**
+   * 1) 데이터를 가공해서 새로운 데이터를 반환할 때
+   * 2) private한 값을 반환할 때
+   */
+  get nameAndBirth(){
+    return `${this.name}-${this.birth}`;
+  }
+
+  set setName(name){ // parameter 하나 필수
+    this.name = name;
+  }
+}
+
+const daeMan = new MyBias('정대만',522);
+console.log(daeMan);
+console.log(daeMan.nameAndBirth);
+// console.log(daeMan.nameAndBirth());이거 아님
+
+daeMan.setName = '밋치';
+// setter는 프로퍼티처럼 불러올 수 있음
+console.log(daeMan);
+
+class MyBias2 {
+  #name; // ES7에서만 가능
+  birth;
+
+  constructor(name, birth) {
+    this.#name = name;
+    this.birth = birth;
+  }
+
+  get name(){
+    return this.#name;
+  }
+
+  set name(name){
+    this.#name = name;
+  }
+}
+
+console.log('-----------------------')
+const daeMan2 = new MyBias2('정대만', 522);
+console.log(daeMan2);
+console.log(daeMan2.name);
+
+daeMan2.name = '밋치';
+console.log(daeMan2.name);
