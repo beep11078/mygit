747b1ec1ce2f9ee28c85f366a156a4ed7b11b9f8
beep11078 <beep11078@gmail.com>
2025-01-15 12:27:55 +0900

    JS-Super

---
+++ Javascript/2_class_and_oop/5_super_n_override.js
@@ -0,0 +1,39 @@
+/**
+ * Super and Override
+ */
+
+class BasketTeam {
+  name;
+  number;
+
+  constructor(name, number) {
+    this.name = name;
+    this.number = number;
+  }
+
+  myName(){
+    return `난 ${this.name}. 포기를 모르는 남자지⋯.`
+  }
+}
+
+class Shohoku extends BasketTeam {
+  position;
+
+  constructor(name, number, position) {
+    super(name, number); // 부모 클래스 (슈퍼 클래스) 상속 == BasketTeam()
+    this.position = position;
+  }
+  
+  myName(){
+    // return `난 ${this.name}. 포기를 모르는 ${this.position}지⋯.`
+    return `${super.myName()} ${this.position}이 다.`
+  }
+}
+
+const daeMan = new Shohoku('정대만', 14, '슈터');
+console.log(daeMan);
+
+const taeWoong = new Shohoku('서태웅', 11);
+console.log(taeWoong);
+console.log(taeWoong.myName());
+console.log(daeMan.myName());
