de1eb335281d0448287cbabdc2cb3676fbb1b64f
beep11078 <beep11078@gmail.com>
2025-01-28 18:21:21 +0900

    DeepDive_생성자 함수

--- _Javascript/0_Deep_Dive/17_new.js
+++ _Javascript/0_Deep_Dive/17_new.js
@@ -1,20 +1,42 @@
-// [예제 17-04]
+// [예제 17-17]
+// 생성자 함수로서 정의하지 않은 일반 함수
+// this로 바인딩 해주지 않아서
+function add(x, y) {
+  return x + y;
+}
+
+// 생성자 함수로서 정의하지 않은 일반 함수를 new 연산자와 함께 호출
+let inst = new add();
+
+// 함수가 객체를 반환하지 않았으므로 반환문이 무시된다. 따라서 빈 객체가 생성되어 반환된다.
+console.log(inst); // add {}
+console.log('-----------')
+
+// 객체를 반환하는 일반 함수
+function createUser(name, role) {
+  return { name, role };
+}
+
+// 일반 함수를 new 연산자와 함께 호출
+inst = new createUser('Lee', 'admin');
+// 함수가 생성한 객체를 반환한다.
+console.log(inst); 
+
+
+// [예제 17-19]
+// 생성자 함수 
 function Circle(radius) {
-  // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킴
+  // 이 함수가 new 연산자와 함께 호출되지 않았다면 new.target은 undefined
+  if(!new.target) {
+    // new 연산자와 함께 생성자 함수를 재귀 호출하여 생성된 인스턴스를 반환한다.
+    return new Circle(radius);
+  }
   this.radius = radius;
   this.getDiameter = function() {
     return 2 * this.radius;
   };
 }
 
-// 인스턴스 생성
-const circle1 = new Circle(5);
-const circle2 = new Circle(10);
-
-console.log(circle1.getDiameter());
-console.log(circle2.getDiameter());
-
-// new 연산자 없 -> 일반 함수로 호출
-const circle3 = Circle(15);
-console.log(circle3); // undefined
-console.log(radius); // 일반 함수의 this = 전역 객체 
+// new 연산자 없이 생성자 함수를 호출해도 new.target을 통해 생성자 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle.getDiameter());
